124164b3e0c021d0c0a1a582617bfb2c8aa3235e
Arun Kumar G <2632288-arung86@users.noreply.gitlab.com>
2020-11-19 11:29:33 +0530

    ebabled chrome debug setting

--- app/src/main/java/com/adbello/android/MainActivity.kt
+++ app/src/main/java/com/adbello/android/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.adbello.android
 
+import android.content.pm.ApplicationInfo
+import android.os.Build
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
+
 class MainActivity : AppCompatActivity() {
     private val webView: WebView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (0 != applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) {
+                WebView.setWebContentsDebuggingEnabled(true)
+            }
+        }
         setContentView(R.layout.activity_main)
         title = "Add Manager"
         val webView = findViewById<WebView>(R.id.webView)
@@ -16,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         webView.loadUrl("http://spoorthiadsbucket.s3-website.ap-south-1.amazonaws.com/#/")
         val webSettings = webView.settings
         webSettings.javaScriptEnabled = true
+
     }
 
     override fun onBackPressed() {
