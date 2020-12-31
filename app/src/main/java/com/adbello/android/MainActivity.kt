ba1d187eaf0adc244af5a2d56694d8b3a5265462
Arun Kumar G <2632288-arung86@users.noreply.gitlab.com>
2020-12-31 07:55:58 +0530

    add download enable

--- app/src/main/java/com/adbello/android/MainActivity.kt
+++ app/src/main/java/com/adbello/android/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.adbello.android
 
+import android.app.DownloadManager
+import android.content.Context
 import android.content.pm.ApplicationInfo
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.os.Environment
+import android.webkit.*
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -25,6 +29,24 @@ class MainActivity : AppCompatActivity() {
         webView.loadUrl("http://spoorthiadsbucket.s3-website.ap-south-1.amazonaws.com/#/")
         val webSettings = webView.settings
         webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+        webView.webChromeClient = WebChromeClient()
+
+        webView.setDownloadListener({ url, userAgent, contentDisposition, mimeType, contentLength ->
+            val request = DownloadManager.Request(Uri.parse(url))
+            request.setMimeType(mimeType)
+            request.addRequestHeader("cookie", CookieManager.getInstance().getCookie(url))
+            request.addRequestHeader("User-Agent", userAgent)
+            request.setDescription("Downloading file...")
+            request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType))
+            request.allowScanningByMediaScanner()
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            request.setDestinationInExternalFilesDir(this@MainActivity, Environment.DIRECTORY_DOWNLOADS, ".png")
+            val dm = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+            dm.enqueue(request)
+            Toast.makeText(applicationContext, "Downloading File", Toast.LENGTH_LONG).show()
+        })
+
 
     }
 
